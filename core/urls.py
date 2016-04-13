9061128585623f975c23ac6acd29d23ed6c282df
serpoletta <margaritalunae@gmail.com>
2016-04-13 15:07:54 +0300

    Fixed errors after refactoring

--- core/urls.py
+++ core/urls.py
@@ -1,23 +1,22 @@
 from django.conf.urls import url
-from . import views
 
 urlpatterns = [
-    url(r'^$', 'views.home', name='home'),
+    url(r'^$', 'core.views.home', name='home'),
     # Юзеры
-    url(r'^user/$', 'views.user_list', name='user_list'),
-    url(r'^user/(\d+)/$', 'views.user_profile', name='user_profile'),
+    url(r'^user/$', 'core.views.user_list', name='user_list'),
+    url(r'^user/(\d+)/$', 'core.views.user_profile', name='user_profile'),
     # ^user/(\d+)/edit/$
 
     # Доски
-    url(r'^board/$', 'views.board_list', name='board_list'),
-    url(r'^board/new/$', 'views.board_new', name='board_new'),
-    url(r'^board/(\d+)/$', 'views.board_detail', name='board_detail'),
-    url(r'^board/(\d+)/edit/$', 'views.board_edit', name='board_edit'),
+    url(r'^board/$', 'core.views.board_list', name='board_list'),
+    url(r'^board/new/$', 'core.views.board_new', name='board_new'),
+    url(r'^board/(\d+)/$', 'core.views.board_detail', name='board_detail'),
+    url(r'^board/(\d+)/edit/$', 'core.views.board_edit', name='board_edit'),
 
     # Карточки
-    url(r'^board/(\d+)/card/$', 'views.card_list', name='card_list'),
-    url(r'^board/(\d+)/card/new/$', 'views.card_new', name='card_new'),
-    url(r'^board/(\d+)/card/(\d+)/$', 'views.card_detail', name='card_detail'),
-    url(r'^board/(\d+)/card/(\d+)/edit/$', 'views.card_edit', name='card_edit'),
+    url(r'^board/(\d+)/card/$', 'core.views.card_list', name='card_list'),
+    url(r'^board/(\d+)/card/new/$', 'core.views.card_new', name='card_new'),
+    url(r'^board/(\d+)/card/(\d+)/$', 'core.views.card_detail', name='card_detail'),
+    url(r'^board/(\d+)/card/(\d+)/edit/$', 'core.views.card_edit', name='card_edit'),
 ]
 
--- pinandplayapp/urls.py
+++ pinandplayapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # url(r'^$', 'pinandplayapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'', include('core.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('core.urls')),
 
 ]
