5c2387f7d1be420465601ea481edd9c7df35f139
serpoletta <margaritalunae@gmail.com>
2016-04-13 13:21:59 +0300

    Add url rules and names for views, renamed pingames to pinandplay

--- core/views.py
+++ core/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Для юзеров
+def user_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+def user_profile(request):
+    return render(request, 'blog/post_list.html', {})
+
+
+# Для досок
+def board_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+def board_detail(request):
+    return render(request, 'blog/post_list.html', {})
+
+def board_new(request):
+    return render(request, 'blog/post_list.html', {})
+
+def board_edit(request):
+    return render(request, 'blog/post_list.html', {})
+
+
+# Для карточек
+def card_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+def card_detail(request):
+    return render(request, 'blog/post_list.html', {})
+
+def card_new(request):
+    return render(request, 'blog/post_list.html', {})
+
+def card_edit(request):
+    return render(request, 'blog/post_list.html', {})
+
