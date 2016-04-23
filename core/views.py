27b470bcf77a7c377e89582d2ee2a31c501b89be
serpoletta <margaritalunae@gmail.com>
2016-04-23 15:14:03 +0300

    Reorginized templates. It works!

--- core/views.py
+++ core/views.py
@@ -56,4 +56,4 @@ def card_edit(request, pk_board, pk_card):
 
 
 def index(request):
-    return render(request, 'tamplates/pinandplay/index.html', {}) #прописан путь о_о
+    return render(request, 'pinandplay/index.html', {}) #прописан путь о_о
